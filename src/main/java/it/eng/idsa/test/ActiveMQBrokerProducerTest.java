3365f8eec83ba1442b6b20fb63bb5393214232f2
Gabriele De Luca <gabriele.deluca@hotmail.com>
2019-08-01 10:08:59 +0200

    File moved

---
+++ src/main/java/it/eng/idsa/test/ActiveMQBrokerProducerTest.java
@@ -0,0 +1,25 @@
+package it.eng.idsa.test;
+
+
+
+import it.eng.idsa.QueueMessageProducer;
+
+
+/**
+* The ActiveMQBrokerProducerTest class creates an ActiveMQ queue for testing
+*
+* @author  Gabriele De Luca, Milan Karajovic
+*/
+public class ActiveMQBrokerProducerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String brokerUrl = "tcp://192.168.56.102:61616";
+		//brokerUrl="ssl://192.168.56.103:61713";
+
+		QueueMessageProducer queProducer = new QueueMessageProducer(brokerUrl, "admin", "admin");
+		queProducer.sendDummyMessages("milan");
+
+	}
+
+}
